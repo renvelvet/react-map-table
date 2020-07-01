5b942549581f079ed9266e767d01ea0bccb8f6b7
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-07-01 15:32:33 +0700

    Create exercise

---
+++ src/components/TableBody/TableBody.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TableBody() {
+    return (
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    );
+}
+
+export default TableBody;
